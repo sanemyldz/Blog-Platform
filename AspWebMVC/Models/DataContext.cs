13f11092f322d38f45f27bde57cedeb23e3d7bd3
sanem <152120181043@ogrenci.ogu.edu.tr>
2022-06-15 12:42:37 +0300

    Login and Sign Up pages implemented

---
+++ AspWebMVC/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspWebMVC.Models;
+
+#nullable disable
+
+namespace AspWebMVC.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> User { get; set; }
+
+    }
+}
---
+++ AspWebMVC/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace AspWebMVC.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string surname { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+}
